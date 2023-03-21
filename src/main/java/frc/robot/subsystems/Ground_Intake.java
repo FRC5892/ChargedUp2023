14e4433c05a3a127b170eadf24f12803386c1221
Micah Guttman <>
2023-03-21 16:33:35 -0500

    cleaning up

--- src/main/java/frc/robot/subsystems/Ground_Intake.java
+++ src/main/java/frc/robot/subsystems/Ground_Intake.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotContainer;
 
 public class Ground_Intake extends SubsystemBase {
 	/* Solenoid */
