de274db5bbc988e7cf39a3c2059874a54d375c10
Chloe Phan <s1658059@online.houstonisd.org>
2023-04-02 17:05:55 -0500

    Speedy balance changes

--- src/main/java/frc/robot/commands/Balance/SpeedyBalance.java
+++ src/main/java/frc/robot/commands/Balance/SpeedyBalance.java
@@ -1,12 +1,10 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.Balance;
 
 import com.ctre.phoenix.sensors.Pigeon2;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Swerve;
@@ -15,20 +13,21 @@ public class SpeedyBalance extends CommandBase {
   private Swerve s_Swerve;
   private Pigeon2 gyro;
   private boolean finish;
-  private double previousAngle;
-  private double currentAngle;
+  private Timer timer;
+  private Timer timer2;
 
-  private boolean foward;
-  private double previousAngleSign;
-  private double goUpDistance;
-  private int snapBackDistance;
+  private double initAngle;
+  private double currentAngle;
+  private double angleDiff;
+  private double initSign;
 
   /** Creates a new ActiceBalance. */
   public SpeedyBalance(Swerve swerve, Pigeon2 gyro) {
     this.s_Swerve = swerve;
     this.gyro = gyro;
+    timer2 = new Timer();
+
     finish = false;
-    foward = true;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(swerve);
   }
@@ -36,59 +35,58 @@ public class SpeedyBalance extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    previousAngle = gyro.getRoll();
-    previousAngleSign = Math.signum(previousAngle);
-    snapBackDistance = 5;
-    goUpDistance = 10;
-    //immediately drive fast
-    if(previousAngleSign == 1.0){
-      s_Swerve.drive(new Translation2d(goUpDistance, 0).times(-Constants.Swerve.speedyBalanceSpeed),
-    0, false, true);
-      foward = true;
-      
-    }
-    else{
-      s_Swerve.drive(new Translation2d(goUpDistance, 0).times(Constants.Swerve.speedyBalanceSpeed),
-    0, false, true);
-    foward = false;
-    }
+    timer2.reset();
+    // immediately drive fast
 
     finish = false;
+    initAngle = gyro.getRoll();
+    initSign = Math.signum(initAngle);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
 
     currentAngle = gyro.getRoll();
-    double delta = previousAngle - currentAngle;
-    if(foward){
-      if(delta>previousAngle){
-        finish=true;
-      }
-    else{
-      if(delta<previousAngle){
-        finish=true;
-      }
-      }
+
+    double delta = currentAngle - initAngle;
+    // previousAngle = currentAngle;
+    SmartDashboard.putNumber("delta", delta);
+    boolean robotTipped = false;
+    if (initSign == 1.0) {
+      robotTipped = delta < -2;
+    } else {
+      robotTipped = delta > 2;
     }
-    
+    // boolean backedUpEnough = timer2.get() > 0.5;
+    int snapBackDistance = 10;
+
+    // drive while timer goes
+    if (robotTipped) {
+      timer2.start();
+      if (timer2.get() < 0.5) {
+        s_Swerve.drive(new Translation2d(1, 0).times(initSign * Constants.Swerve.speedyBackup),
+            0, false, true);
+      } else {
+        timer2.stop();
+        finish = true;
+      }
+    } else {
+      s_Swerve.drive(new Translation2d(1, 0).times(-initSign * 2),
+          0, false, true);
+    }
+
+    // if (backedUpEnough) {
+    // s_Swerve.drive(new Translation2d(0, 0).times(0),
+    // 0, false, true);
+    // finish = true;
+    // }
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    if(foward){
-    s_Swerve.drive(
-          new Translation2d(snapBackDistance, 0).times(Constants.Swerve.speedyBackup),
-          0, false, true);
-    }
-    else{
-      s_Swerve.drive(
-          new Translation2d(snapBackDistance, 0).times(-Constants.Swerve.speedyBackup),
-          0, false, true);
-    }
   }
 
   // Returns true when the command should end.
